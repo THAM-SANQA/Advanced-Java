1f4cda99faba511489c1245365f771de056ef094
THAM-SANQA <ndabat020@gmail.com>
2024-02-09 13:10:08 +0200

    update lesson 10

---
+++ lessons/src/lesson10/TestCollectCount.java
@@ -0,0 +1,25 @@
+package lesson10;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author TJ
+ */
+public class TestCollectCount {
+
+    public static void main(String[] args) {
+
+        List<Employee> eList = Employee.createShortList();
+
+        Map<String, Long> gMap = eList.stream()
+                .collect(Collectors.groupingBy(e -> e.getDept(), Collectors.counting()));
+
+        System.out.println("\n== Employees by Dept ==");
+        gMap.forEach((k, v) -> System.out.println("Department: " + k + ", Number of employees: " + v));
+
+    }
+
+}
