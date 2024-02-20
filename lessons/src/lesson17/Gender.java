a6a73a408442d278c0759792fbba0c541145a7ad
THAM-SANQA <ndabat020@gmail.com>
2024-02-20 23:03:31 +0200

    update practice 6

---
+++ lessons/src/lesson17/Gender.java
@@ -0,0 +1,6 @@
+package lesson17;
+
+/**
+ * @author MikeW
+ */
+public enum Gender { MALE, FEMALE }
---
+++ lessons/src/lesson17/Role.java
@@ -0,0 +1,6 @@
+package lesson17;
+
+/**
+ * @author MikeW
+ */
+public enum Role { STAFF, MANAGER, EXECUTIVE }
