23f3085f1b832a36712c42adb4befc1b5065f623
THAM-SANQA <ndabat020@gmail.com>
2024-02-23 09:56:42 +0200

    update practice 6

--- lessons/src/lesson18/practices/SimpleJDBCExample.java
+++ lessons/src/lesson18/practices/SimpleJDBCExample.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
 
+@SuppressWarnings("ALL")
 public class SimpleJDBCExample {
 
     public static void main(String[] args) {
